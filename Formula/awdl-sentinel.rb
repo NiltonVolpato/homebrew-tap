883c9d2711382f795eb2a234a12bfc3e31cd96bb
Nilton Volpato <nilton@volpa.to>
2026-09-01 18:45:32 -0700

    Update for version v1.0.0
    Signed-off-by: Nilton Volpato <nilton@volpa.to>

--- Formula/awdl-sentinel.rb
+++ Formula/awdl-sentinel.rb
@@ -1,10 +1,10 @@
 class AwdlSentinel < Formula
   desc "Kernel-level sentinel to suppress AWDL-caused WiFi latency trashing"
   homepage "https://github.com/NiltonVolpato/awdl-sentinel/"
-  url "https://github.com/NiltonVolpato/awdl-sentinel/archive/refs/tags/v0.0.0.tar.gz"
-  sha256 "a1fbf7de348831d3c677c1b555b142893beda9c2e6fd3ea82084343e1af78c21"
+  url "https://github.com/NiltonVolpato/awdl-sentinel/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "0ac3b1af541d273d0f15b3f01c43ad1b2a6e71fc076dadbbad070eafb3288212"
   license "Apache-2.0"
-  head "https://github.com/NiltonVolpato/awdl-sentinel.git"
+  head "https://github.com/NiltonVolpato/awdl-sentinel.git", :using => :git, branch: "main"
 
   depends_on xcode: ["14.0", :build]
   depends_on :macos
@@ -17,7 +17,7 @@ class AwdlSentinel < Formula
   end
 
   service do
-    run [opt_bin/"awdl-sentinel"]
+    run [opt_bin/"awdl-sentinel", "daemon"]
     require_root true
     keep_alive true
     log_path var/"log/awdl-sentinel.log"
